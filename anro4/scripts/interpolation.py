c82a6bcbd9f0298c644fd4374a539c9707bd34a5
Linux <adam.solawa@gmail.com>
2020-05-09 15:08:35 +0200

    Add jcmd, refactor srv, fix interpol cal

--- anro4/scripts/interpolation.py
+++ anro4/scripts/interpolation.py
@@ -1,25 +1,25 @@
 #! /usr/bin/env python
 
 import rospy
-from anro4.srv import interpol
+from anro4.srv import Interpol
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Header
 import math
 
-f=50
+f=10
 actual_pos=[0,0,0]
 new_pos=[0,0,0]
 
 def interpolation_function(data):
 
     if data.time<=0:
-        return False
+        return {"status": False, "msg": "Wrong time value"}
     if -1.57 >= data.j1 or data.j1>=1.57:
-        return False
+        return {"status": False, "msg": "Wrong 1rd joint value"}
     if -1.57 >= data.j2 or data.j2>=1.57:
-        return False
+        return {"status": False, "msg": "Wrong 2rd joint value"}
     if  data.j3<=0.05 or data.j3>=0.16:
-        return False
+        return {"status": False, "msg": "Wrong 3rd joint value"}
 
     actual_pos=rospy.wait_for_message('joint_states',JointState,timeout=10).position
     new_pos=[data.j1,data.j2,data.j3]
@@ -29,27 +29,24 @@ def interpolation_function(data):
     j3=actual_pos[2]
 
     frames_num= int(math.ceil(data.time*f))
-    actual_time=0
 
-    for i in range(0,frames_num+1):
+    for actual_frame in range(0,frames_num+1):
         calculatedJointState=JointState()
         calculatedJointState.header=Header()
         calculatedJointState.header.stamp=rospy.Time.now()
         calculatedJointState.name=['base_to_link1','link1_to_link2','link2_to_link3']
 
-        j1=linear_interpolation(actual_pos[0],new_pos[0],data.time,actual_time)
-        j2=linear_interpolation(actual_pos[1],new_pos[1],data.time,actual_time)
-        j3=linear_interpolation(actual_pos[2],new_pos[2],data.time,actual_time)
+        j1=linear_interpolation(actual_pos[0],new_pos[0],frames_num,actual_frame)
+        j2=linear_interpolation(actual_pos[1],new_pos[1],frames_num,actual_frame)
+        j3=linear_interpolation(actual_pos[2],new_pos[2],frames_num,actual_frame)
 
         calculatedJointState.position=[j1,j2,j3]
         calculatedJointState.velocity= []
         calculatedJointState.effort= []
         pub.publish(calculatedJointState)
-        actual_time+=1
         rate.sleep()
 
-
-    return True
+    return {"status": True, "msg": "Interpolation finished successfully"}
 
 
 def linear_interpolation(start,end,time,actual_time):
@@ -66,7 +63,7 @@ if __name__ == '__main__':
     print("File works")
     rospy.init_node('interpol_srv', anonymous=True)
     pub = rospy.Publisher('interpolation', JointState, queue_size=10)
-    s=rospy.Service('interpol',interpol,interpolation_function)
+    s=rospy.Service('interpol',Interpol,interpolation_function)
 
 
     rospy.spin()
